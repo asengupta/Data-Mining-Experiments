02fb4554f9a0ad9232f4b4317e7761d9458dc778
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-08-01 16:11:40 +0530

    Pushing table creation and score calculations as columns.

--- db_migrator.rb
+++ db_migrator.rb
@@ -6,6 +6,8 @@ include BitwiseOperations
 handle = File.open('/home/avishek/BitwiseOperations/Ang2010TestsModified.csv', 'r')
 my = Mysql::new("localhost", "root", "", "data_mining")
 
+my.query('CREATE TABLE responses (student_id int(11) DEFAULT NULL, area char(50) DEFAULT NULL, pre_performance bigint(20) DEFAULT NULL, post_performance bigint(20) DEFAULT NULL, language char(50) DEFAULT NULL, gender char(20) DEFAULT NULL, pre_total int, post_total int, id int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`));')
+
 inputs = []
 handle.each_line do |line|
 	split_elements = line.split('|')
@@ -27,9 +29,11 @@ handle.each_line do |line|
 		    	:before => pre_response_as_64, 
 		    	:after => post_response_as_64, 
 		    	:language => split_elements[5], 
-		    	:gender => split_elements[4]
+		    	:gender => split_elements[4],
+			:pre_total => bitcount_64(pre_response_as_64),
+			:post_total => bitcount_64(post_response_as_64)
 		  }
-	query = "insert into responses (student_id, area, language, gender, pre_performance, post_performance) values (#{input[:student_id]}, '#{input[:area]}', '#{input[:language]}', '#{input[:gender]}', #{input[:before]}, #{input[:after]});"
+	query = "insert into responses (student_id, area, language, gender, pre_performance, post_performance, pre_total, post_total) values (#{input[:student_id]}, '#{input[:area]}', '#{input[:language]}', '#{input[:gender]}', #{input[:before]}, #{input[:after]}, #{input[:pre_total]}, #{input[:post_total]});"
 	my.query(query)
 
 end
