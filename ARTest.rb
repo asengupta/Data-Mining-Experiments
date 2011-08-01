71b5d72031758ffc9fbe2b2c7228dadeabbd4bb4
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-08-01 15:28:45 +0530

    Adding some scoring semantics.

--- ARTest.rb
+++ ARTest.rb
@@ -5,4 +5,6 @@ include BitwiseOperations
 
 response = Response.find(55093)
 p response.inspect
+p response.post_score
+
 
--- schema.rb
+++ schema.rb
@@ -1,6 +1,16 @@
 require 'db_config'
+require 'BitwiseOperations'
+
+include BitwiseOperations
+
 
 class Response < ActiveRecord::Base
-end
+	def pre_score
+		bitcount_64(pre_performance.to_i)
+	end
 
+	def post_score
+		bitcount_64(post_performance.to_i)
+	end
+end
 
