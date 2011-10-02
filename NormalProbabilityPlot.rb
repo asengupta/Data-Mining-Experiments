8d74bcde096ffc8bbb857a696901467a4bf83c40
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-10-02 23:24:30 +0530

    Adding residual plot, but will remove in next commit

--- NormalProbabilityPlot.rb
+++ NormalProbabilityPlot.rb
@@ -6,6 +6,7 @@ ENV['GEM_PATH'] = '/home/avishek/jruby/jruby-1.6.3/lib/ruby/gems/1.8'
 require 'schema'
 require 'basis_processing'
 require 'quantiles'
+require 'distributions'
 
 class MySketch < Processing::App
 	app = self
@@ -15,7 +16,7 @@ class MySketch < Processing::App
 		@screen_height = 900
 		@width = width
 		@height = height
-		@screen_transform = Transform.new({:x => 5.0, :y => -5.0}, {:x => 600.0, :y => @screen_height / 2})
+		@screen_transform = Transform.new({:x => 5.0, :y => -5.0}, {:x => 1200.0, :y => @screen_height / 2})
 		@screen = Screen.new(@screen_transform, self)
 		frame_rate(30)
 		smooth
@@ -23,12 +24,15 @@ class MySketch < Processing::App
 		color_mode(RGB, 1.0)
 
 		responses = Response.find(:all)
+		cumulative_improvement_bins = {}
 		improvement_bins = {}
 		data_bins = {}
 		normal_bins = {}
 		responses.each do |r|
-			improvement_bins[metric.call(r)] = responses.select {|rsp| metric.call(rsp) <= metric.call(r)}.count/responses.count.to_f * 100.0 if improvement_bins[metric.call(r)] == nil
+			cumulative_improvement_bins[metric.call(r)] = responses.select {|rsp| metric.call(rsp) <= metric.call(r)}.count/responses.count.to_f * 100.0 if cumulative_improvement_bins[metric.call(r)] == nil
+			improvement_bins[metric.call(r)] = improvement_bins[metric.call(r)] == nil ? 1 : improvement_bins[metric.call(r)] + 1
 		end
+		improvement_bins = improvement_bins.each_pair {|k,v| improvement_bins[k] = v/responses.count.to_f * 100.0}
 		
 		value_sum = 0
 		responses.each do |r|
@@ -42,15 +46,13 @@ class MySketch < Processing::App
 		variance = sum_of_squares.to_f/responses.count
 
 		quantile_fn = Quantiles.quantile_normal(mean, variance)
-		improvement_bins.each_pair do |improvement, percentage|
+		cumulative_improvement_bins.each_pair do |improvement, percentage|
 			normal_bins[percentage] = quantile_fn.call(percentage/100.0)
 			data_bins[percentage] = improvement
 		end
-		least_improvement = improvement_bins.keys.min
-		most_improvement = improvement_bins.keys.max
+		least_improvement = cumulative_improvement_bins.keys.min
+		most_improvement = cumulative_improvement_bins.keys.max
 
-		p least_improvement
-		p most_improvement
 		@x_unit_vector = {:x => 1.0, :y => 0.0}
 		@y_unit_vector = {:x => 0.0, :y => 1.0}
 
@@ -68,13 +70,32 @@ class MySketch < Processing::App
 			no_fill()
 			@screen.plot({:x => normal_bins[p], :y => normal_bins[p]}, @c) { |p| rect(p[:x], p[:y], 4, 4)}
 		end
+		normal_distribution = Distributions.normal(mean, variance)
+		residual_hash = {}
+		improvement_bins.each_key do |improvement|
+			residual = - normal_distribution.call(improvement) * 100.0 + cumulative_improvement_bins[improvement]
+#			p "#{normal_distribution.call(improvement) * 100.0} == #{cumulative_improvement_bins[improvement]}"
+			residual_hash[improvement] = residual
+		end
+		p "Minimum = #{residual_hash.values.min}"
+		p "Maximum = #{residual_hash.values.max}"
+		x_range = ContinuousRange.new({:minimum => residual_hash.keys.min, :maximum => residual_hash.keys.max})
+		y_range = ContinuousRange.new({:minimum => residual_hash.values.min, :maximum => residual_hash.values.max})
+		@c = CoordinateSystem.new(Axis.new(@x_unit_vector,x_range), Axis.new(@y_unit_vector,y_range), [[1,0],[0,1]], self)
+		@residual_screen_transform = Transform.new({:x => 5.0, :y => -5.0}, {:x => 300.0, :y => @screen_height})
+		@residuals_screen = Screen.new(@residual_screen_transform, self)
+
+		improvement_bins.each_pair do |improvement, residual|
+			@residuals_screen.plot({:x => improvement, :y => residual}, @c)
+		end
+		@residuals_screen.draw_axes(@c, 10, 2.0)
 	end
 
 	def draw
 	end
 end
 
-w = 1200
+w = 2000
 h = 1000
 
 MySketch.new(:title => "Normal Probability Plot", :width => w, :height => h)
