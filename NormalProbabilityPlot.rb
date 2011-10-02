22305dc615ec8dafd0a736a8a3229a75b4276a76
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-10-02 16:48:20 +0530

    Adding normal probability plot, now uses quantile approximation

--- NormalProbabilityPlot.rb
+++ NormalProbabilityPlot.rb
@@ -5,19 +5,16 @@ ENV['GEM_PATH'] = '/home/avishek/jruby/jruby-1.6.3/lib/ruby/gems/1.8'
 
 require 'schema'
 require 'basis_processing'
+require 'quantiles'
 
 class MySketch < Processing::App
 	app = self
 	
-	def normal(mean, variance)
-		lambda {|x| 1.0/(Math.sqrt(2.0 * Math::PI * variance)) * Math.exp(-((x - mean)**2) / (2 * variance))}
-	end
-
 	def setup
 		@screen_height = 900
 		@width = width
 		@height = height
-		@screen_transform = Transform.new({:x => 10.0, :y => -5.0}, {:x => 600.0, :y => @screen_height})
+		@screen_transform = Transform.new({:x => 5.0, :y => -5.0}, {:x => 600.0, :y => @screen_height / 2})
 		@screen = Screen.new(@screen_transform, self)
 		frame_rate(30)
 		smooth
@@ -25,16 +22,18 @@ class MySketch < Processing::App
 		color_mode(RGB, 1.0)
 
 		responses = Response.find(:all)
-#		p responses.select {|r| r[:pre_total] == 0 && r[:post_total] > 50}
-#		exit
-
-		dis = normal(4, 245)
 		improvement_bins = {}
+		data_bins = {}
+		normal_bins = {}
 		responses.each do |r|
-			improvement = r[:post_total] - r[:pre_total]
-			improvement_bins[improvement] = responses.select {|r| r[:post_total] - r[:pre_total] <= improvement}.count/responses.count.to_f * 100.0 if improvement_bins[improvement] == nil
+			improvement_bins[r.improvement] = responses.select {|rsp| rsp.improvement <= r.improvement}.count/responses.count.to_f * 100.0 if improvement_bins[r.improvement] == nil
 		end
-
+		
+		quantile_fn = Quantiles.quantile_normal(4, 260)
+		improvement_bins.each_pair do |improvement, percentage|
+			normal_bins[percentage] = quantile_fn.call(percentage/100.0)
+			data_bins[percentage] = improvement
+		end
 		least_improvement = improvement_bins.keys.min
 		most_improvement = improvement_bins.keys.max
 
@@ -42,14 +41,18 @@ class MySketch < Processing::App
 		@y_unit_vector = {:x => 0.0, :y => 1.0}
 
 		x_range = ContinuousRange.new({:minimum => least_improvement, :maximum => most_improvement})
-		y_range = ContinuousRange.new({:minimum => 0.0, :maximum => 100.0})
+		y_range = ContinuousRange.new({:minimum => least_improvement, :maximum => most_improvement})
 
 		@c = CoordinateSystem.new(Axis.new(@x_unit_vector,x_range), Axis.new(@y_unit_vector,y_range), [[1,0],[0,1]], self)
-		@screen.draw_axes(@c,5,5)
-		stroke(1,1,0,1)
-		fill(1,1,0)
-		improvement_bins.each_pair do |improvement,percentage|
-			@screen.plot({:x => improvement, :y => percentage}, @c)
+		@screen.draw_axes(@c,10,10)
+		rect_mode(CENTER)
+		normal_bins.each_key do |p|
+			stroke(1,1,0,1)
+			fill(1,1,0)
+			@screen.plot({:x => normal_bins[p], :y => data_bins[p]}, @c)
+			stroke(0,1,0,1)
+			no_fill()
+			@screen.plot({:x => normal_bins[p], :y => normal_bins[p]}, @c) { |p| rect(p[:x], p[:y], 4, 4)}
 		end
 	end
 
@@ -60,6 +63,6 @@ end
 w = 1200
 h = 1000
 
-MySketch.new(:title => "My Sketch", :width => w, :height => h)
+MySketch.new(:title => "Normal Probability Plot", :width => w, :height => h)
 
 
---
+++ quantiles.rb
@@ -0,0 +1,16 @@
+module Quantiles
+	def self.quantile(p)
+	end
+	
+	def self.erf_inverse(x)
+		a = 0.147
+		common_term = (2/(Math::PI * a)) + Math.log(1.0 - x**2)
+		term2 = Math.log(1.0 - x**2)/a
+		(x<=>0.0) * Math.sqrt(Math.sqrt(common_term**2 - term2) - common_term)
+	end
+	
+	def self.quantile_normal(mean, variance)
+		lambda {|p| mean + Math.sqrt(2 * variance) * erf_inverse(2 * p - 1.0)}
+	end
+end
+
