3eed0208ecffe2797f0a3b4f06ecf20f07e8c9c3
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-10-10 22:19:09 +0530

    Cleaning up box plot

--- BoxPlot.rb
+++ BoxPlot.rb
@@ -26,22 +26,23 @@ class MySketch < Processing::App
 		end
 		
 		bins["ALL"] = responses
-		p bins.keys.inspect
+
 		bins.each_pair do |k,v|
 			begin
 				bins[k] = box(k, v, metric)
 			rescue => e
-				puts "Deleting #{k}...statistically insignificant"
-				bins.delete(k)
+				puts "Warning: Population of #{k} is statistically insignificant"
+				bins[k] = nil
 			end
 		end
+		p bins.inspect
 
 #		box = {:minimum => 20, :maximum => 70, :q1 => 30, :q2 => 40, :q3 => 50}
 
 		@x_unit_vector = {:x => 1.0, :y => 0.0}
 		@y_unit_vector = {:x => 0.0, :y => 1.0}
-		@screen_transform = Transform.new({:x => 5.0, :y => -10.0}, {:x => 100, :y => @screen_height})
-		x_range = ContinuousRange.new({:minimum => 0.0, :maximum => 360.0})
+		@screen_transform = Transform.new({:x => 4.0, :y => -6.0}, {:x => 100, :y => @screen_height})
+		x_range = ContinuousRange.new({:minimum => 0.0, :maximum => 500.0})
 		y_range = ContinuousRange.new({:minimum => 0.0, :maximum => 100.0})
 		@c = CoordinateSystem.new(Axis.new(@x_unit_vector,x_range), Axis.new(@y_unit_vector,y_range), self, [[1,0],[0,1]])
 		@screen = Screen.new(@screen_transform, self, @c)
@@ -53,12 +54,17 @@ class MySketch < Processing::App
 		
 		stroke_weight(1)
 		stroke(0.7,0,0)
-		@screen.draw_axes(10, 10, {:x => lambda {|p| ''}, :y => lambda {|p| p.to_i}})
+		@screen.draw_axes(10, 10, {:x => ->(p){''}, :y => ->(p){p.to_i}})
 
 		stroke(0.7,0.3,1)
 		no_fill
 		bins.keys.sort.each do |k|
+			@screen.at({:x => position, :y => -5}) {|o,m,s| text(k, m[:x] - 15, m[:y] + 30)}
 			v = bins[k]
+			if (v.nil?)
+				position += 25
+				next
+			end
 			@screen.at(v) do |o,s|
 				s.in_basis do
 					stroke_weight(0.2)
@@ -76,7 +82,6 @@ class MySketch < Processing::App
 			@screen.at({:x => position, :y => v[:q1]}) {|o,m,s| text(o[:y], m[:x] + 5, m[:y] + 14)}
 			@screen.at({:x => position, :y => v[:q2]}) {|o,m,s| text(o[:y], m[:x] + 5, m[:y] + 14)}
 			@screen.at({:x => position, :y => v[:q3]}) {|o,m,s| text(o[:y], m[:x] + 5, m[:y] - 14)}
-			@screen.at({:x => position, :y => -5}) {|o,m,s| text(k, m[:x] - 15, m[:y])}
 			
 			position += 25
 		end
@@ -116,7 +121,7 @@ class MySketch < Processing::App
 	end
 	
 	def nth_quartile(n, cdf)
-			cdf.keys[cdf.keys.index {|k| (cdf[k] - (n/4.0)).abs < 0.09 }]
+		cdf.keys[cdf.keys.index {|k| (cdf[k] - (n/4.0)).abs < 0.04 }]
 	end
 end
 
