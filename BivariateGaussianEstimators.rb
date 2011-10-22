d0825bdcfc924a9c0bc481e475654cba6e6ef78e
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-10-22 07:05:11 +0530

    Bigger rectangles

--- BivariateGaussianEstimators.rb
+++ BivariateGaussianEstimators.rb
@@ -17,12 +17,18 @@ class MySketch < Processing::App
 		mean
 	end
 	
+	def variance(samples, mean_x, mean_y, x_metric, y_metric)
+		sum = 0.0
+		samples.each {|s| sum += (x_metric.call(s) - mean_x)*(y_metric.call(s) - mean_y)}
+		sum/samples.count
+	end
+
 	def setup
 		@screen_height = 900
 		@width = width
 		@height = height
 #		@screen_transform = Transform.new({:x => 18.0, :y => -7500.0}, {:x => 100.0, :y => @screen_height})
-		@screen_transform = Transform.new({:x => 10.0, :y => -750.0}, {:x => 500, :y => @screen_height})
+		@screen_transform = Transform.new({:x => 10.0, :y => -10.0}, {:x => 500, :y => @screen_height})
 		smooth
 		no_loop
 		background(0,0,0)
@@ -33,46 +39,44 @@ class MySketch < Processing::App
 		mean_x = mean(responses) {|r| r[:pre_total]}
 		mean_y = mean(responses) {|r| r[:post_total]}
 		
-		mean = [[mean_x], [mean_y]]
-		variance_x = variance(responses, ->(r){r[:pre_total]}, ->(r){r[:pre_total]})
-		variance_y = variance(responses, ->(r){r[:post_total]}, ->(r){r[:post_total]})
-		variance_xy = variance(responses, ->(r){r[:pre_total]}, ->(r){r[:post_total]})
+		mean = Matrix.columns([[mean_x, mean_y]])
+		variance_x = variance(responses, mean_x, mean_y, ->(r){r[:pre_total]}, ->(r){r[:pre_total]})
+		variance_y = variance(responses, mean_x, mean_y, ->(r){r[:post_total]}, ->(r){r[:post_total]})
+		variance_xy = variance(responses, mean_x, mean_y, ->(r){r[:pre_total]}, ->(r){r[:post_total]})
 		
-		variance = [[variance_x, variance_xy], [variance_xy, variance_y]]
+		variance_matrix = Matrix.rows([[variance_x, variance_xy], [variance_xy, variance_y]])
 		gaussian = lambda do |x|
-			(1.0/(2*Math::PI*Math.sqrt(variance.determinant))) * Math.exp(-0.5*(x.minus(mean).transpose*variance.inverse*x.minus(mean)).value)
+			x = x.as_matrix
+			wut = (x - mean).transpose
+			wut *= variance_matrix.inverse
+			wut *= (x - mean)
+			(1.0/(2*Math::PI*Math.sqrt(variance_matrix.determinant))) * Math.exp(-0.5*wut[0,0])
 		end
 
-
-#		@c = CoordinateSystem.standard({:minimum => -60, :maximum => 60}, {:minimum => -60, :maximum => 60}, self)
-#		@screen = Screen.new(@screen_transform, self, @c)
-
-#		stroke(0.1,0.5,1)
-#		fill(0.1,0.5,1)
+		x_start = mean_x - 70
+		x_end = mean_x + 70
+		y_start = mean_y - 70
+		y_end = mean_y + 70
+		y = y_start
 		
+		@c = CoordinateSystem.standard({:minimum => -60, :maximum => 60}, {:minimum => -60, :maximum => 60}, self)
+		@screen = Screen.new(@screen_transform, self, @c)
+		rect_mode(CENTER)
+		while y <= y_end
+			x = x_start
+			while x <= x_end
+				p = {:x => x, :y => y}
+				v = gaussian.call(p)
+				stroke(0.1,0.5,0.3)
+				fill(0.1,0.5,v*700)
+				@screen.plot(p) {|o,m,s| rect(m[:x], m[:y], 10, 10)}
+				x += 1.0
+			end
+			y += 1.0
+		end
+		@screen.draw_axes(10,10)
 	end
 
-	def plot_distribution(responses, metric)
-		improvement_bins = {}
-		responses.each do |r|
-			improvement = metric.call(r)
-			improvement_bins[improvement] = improvement_bins[improvement] == nil ? 1 : improvement_bins[improvement] + 1
-		end
-		
-		improvement_bins.each_pair do |k, v|
-			improvement_bins[k] = v / responses.count.to_f
-		end
-
-		least_improvement = improvement_bins.keys.min
-		most_improvement = improvement_bins.keys.max
-
-		@screen.join = true
-		improvement_bins.keys.sort.each do|k|
-			@screen.plot({:x => k, :y => improvement_bins[k]})
-		end
-		@screen.join = false
-	end
-	
 	def draw
 	end
 end
