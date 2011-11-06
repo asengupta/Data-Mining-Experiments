fc0fa771a094eba0d9d2391fa5f30c10628619e7
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-11-06 14:43:31 +0530

    McNemar test breakdown by cluster

--- McNemarTest.rb
+++ McNemarTest.rb
@@ -12,39 +12,61 @@ ActiveRecord::Base.establish_connection(
 class Response < ActiveRecord::Base
 end
 
+class School < ActiveRecord::Base
+end
+
 def fill_contingency(responses, tables)
 	responses.each do |r|
 		pre_bitstring = r[:pre_performance].to_s(2).rjust(56, '0')
 		post_bitstring = r[:post_performance].to_s(2).rjust(56, '0')
 		i = 0
 		while i <= 55
-			tables[i][:k0_0] = tables[i][:k0_0] + 1 if pre_bitstring[i] == '0' && post_bitstring[i] == '0'
-			tables[i][:k0_1] = tables[i][:k0_1] + 1 if pre_bitstring[i] == '0' && post_bitstring[i] == '1'
-			tables[i][:k1_0] = tables[i][:k1_0] + 1 if pre_bitstring[i] == '1' && post_bitstring[i] == '0'
-			tables[i][:k1_1] = tables[i][:k1_1] + 1 if pre_bitstring[i] == '1' && post_bitstring[i] == '1'
+			tables[i][:d] = tables[i][:d] + 1 if pre_bitstring[i] == '0' && post_bitstring[i] == '0'
+			tables[i][:c] = tables[i][:c] + 1 if pre_bitstring[i] == '0' && post_bitstring[i] == '1'
+			tables[i][:b] = tables[i][:b] + 1 if pre_bitstring[i] == '1' && post_bitstring[i] == '0'
+			tables[i][:a] = tables[i][:a] + 1 if pre_bitstring[i] == '1' && post_bitstring[i] == '1'
 			i += 1
 		end
 	end
 end
 
+def mc_nemar(responses)
+	contingency_tables = []
+	56.times {contingency_tables << {:a => 0, :b => 0, :c => 0, :d => 0}}
+
+	fill_contingency(responses, contingency_tables)
+
+	i = 1
+	statistics = []
+	contingency_tables.each do |table|
+		before_1 = table[:b] + table[:a]
+		before_0 = table[:d] + table[:c]
+		after_1 = table[:c] + table[:a]
+		after_0 = table[:d] + table[:b]
+
+		b = table[:b]
+		c = table[:c]
+		mc_nemar_statistic = (((b-c).abs() -0.5)**2).to_f/(b+c)
+		statistics << mc_nemar_statistic
+		i += 1
+	end
+	statistics
+end
+
+schools = School.find(:all)
 responses = Response.find(:all)
 
-contingency_tables = []
-56.times {contingency_tables << {:k1_1 => 0, :k1_0 => 0, :k0_1 => 0, :k0_0 => 0}}
-
-fill_contingency(responses, contingency_tables)
-
-i = 1
-contingency_tables.each do |table|
-	before_1 = table[:k1_0] + table[:k1_1]
-	before_0 = table[:k0_0] + table[:k0_1]
-	after_1 = table[:k0_1] + table[:k1_1]
-	after_0 = table[:k0_0] + table[:k1_0]
-
-	b = table[:k1_0]
-	c = table[:k0_1]
-	mc_nemar_statistic = (((b-c).abs() -0.5)**2).to_f/(b+c)
-	puts "#{i} & #{mc_nemar_statistic} \\\\"
-	i += 1
+clusters = {}
+responses.each do |r|
+	cluster = schools[schools.index {|s| s[:school_name] == r[:area]}][:cluster]
+	clusters[cluster] = [] if clusters[cluster].nil?
+	clusters[cluster] << r
 end
+
+cluster_statistics = {}
+clusters.each_pair do |cluster, responses|
+	cluster_statistics[cluster] = mc_nemar(responses)
+end
+
+puts cluster_statistics.inspect
 
