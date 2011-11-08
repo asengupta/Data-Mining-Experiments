5c8214907ccce8e079117c35e94e02375de7a4d7
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-11-08 23:01:30 +0530

    OK

--- MatrixMultiplication.rb
+++ MatrixMultiplication.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'statsample'
+require 'benchmark'
 
 class Matrix
 	def block(block_row, block_column)
@@ -25,10 +26,22 @@ def product(a,b)
 		[a[0,0]*b[0,0] + a[0,1]*b[1,0], a[0,0]*b[0,1] + a[0,1]*b[1,1]], 
 		[a[1,0]*b[0,0] + a[1,1]*b[1,0], a[1,0]*b[0,1] + a[1,1]*b[1,1]]])
 	end
+#	tp00 = Thread.new{ Thread.current[:result] = product(a.block(0,0), b.block(0,0)) + product(a.block(0,1), b.block(1,0))}
+#	tp01 = Thread.new{ Thread.current[:result] = product(a.block(0,0), b.block(0,1)) + product(a.block(0,1), b.block(1,1))}
+#	tp10 = Thread.new{ Thread.current[:result] = product(a.block(1,0), b.block(0,0)) + product(a.block(1,1), b.block(1,0))}
+#	tp11 = Thread.new{ Thread.current[:result] = product(a.block(1,0), b.block(0,1)) + product(a.block(1,1), b.block(1,1))}
+	
 	p00 = product(a.block(0,0), b.block(0,0)) + product(a.block(0,1), b.block(1,0))
 	p01 = product(a.block(0,0), b.block(0,1)) + product(a.block(0,1), b.block(1,1))
 	p10 = product(a.block(1,0), b.block(0,0)) + product(a.block(1,1), b.block(1,0))
 	p11 = product(a.block(1,0), b.block(0,1)) + product(a.block(1,1), b.block(1,1))
+
+#	tp00.join
+#	tp01.join
+#	tp10.join
+#	tp11.join
+
+#	Matrix.rows(join(tp00[:result], tp01[:result]) + join(tp10[:result], tp11[:result]))
 	Matrix.rows(join(p00, p01) + join(p10, p11))
 end
 
@@ -41,21 +54,30 @@ def join(left_block, right_block)
 	rows
 end
 
-m1 = Matrix.rows([
-			[1,2,3,4], 
-			[3,5,4,5], 
-			[13,25,14,15], 
-			[23,25,14,5]
-		])
-m2 = Matrix.rows([
-			[12,13,12,7], 
-			[14,15,4,6],
-			[4,5,14,6],
-			[9,5,8,1]
-		])
+order = 128
+m1 = Matrix.build(order, order) {|row, col| rand }
+m2 = Matrix.build(order, order) {|row, col| rand }
+#m1 = Matrix.rows([
+#			[1,2,3,4], 
+#			[3,5,4,5], 
+#			[13,25,14,15], 
+#			[23,25,14,5]
+#		])
+#m2 = Matrix.rows([
+#			[12,13,12,7], 
+#			[14,15,4,6],
+#			[4,5,14,6],
+#			[9,5,8,1]
+#		])
 
 
-puts product(m1,m2)
-puts m1*m2
+threaded_time = Benchmark.measure do
+	product(m1,m2)
+end
+puts "Threaded version = #{threaded_time}"
 
+unthreaded_time = Benchmark.measure do
+	m1*m2
+end
+puts "Unthreaded version = #{unthreaded_time}"
 
